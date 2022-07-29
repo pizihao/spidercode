c8f0621d1d47418b8ecce71b10a56f48ee9723ca
pizihao <2335715300@qq.com>
2022-07-29 19:58:32 +0800

    空的类，添加一个class

--- pizihao/src/main/java/com/deep/service/TestModel.java
+++ pizihao/src/main/java/com/deep/service/TestModel.java
@@ -1,21 +1,34 @@
 package com.deep.service;
 
-import com.deep.jsr269.Api;
-import com.deep.jsr269.Top;
+import com.deep.jsr269.Type;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <h2></h2>
  *
  * @author Create by liuwenhao on 2022/7/28 17:25
  */
-@Top(Api.class)
 public class TestModel {
 
-    private String name;
-    private String age;
+    @Type
+    private List<String> strings = new ArrayList();
+    @Type
+    private Map<? extends Integer, ? super String> integers = new HashMap<>();
 
     public static void main(String[] args) {
+
+        @Type
+        List<String> strings = new ArrayList<>();
         TestModel testModel = new TestModel();
+    }
+
+    public List<String> _stringsType() {
+        return this.strings;
     }
 
 }
