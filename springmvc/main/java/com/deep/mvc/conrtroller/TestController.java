6df22375656e176852a19375fb0e8be4a35f88a6
pizihao <2335715300@qq.com>
2022-08-20 19:06:49 +0800

    拦截器的使用

--- springmvc/main/java/com/deep/mvc/conrtroller/TestController.java
+++ springmvc/main/java/com/deep/mvc/conrtroller/TestController.java
@@ -1,6 +1,13 @@
 package com.deep.mvc.conrtroller;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <h2></h2>
@@ -12,9 +19,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("Test")
 public class TestController {
 
-    @GetMapping("/{id}")
-    public String test(@PathVariable String id){
-        return id;
+    @GetMapping(value = "/{id}")
+    public DeferredResult<String> test(@PathVariable String id,HttpServletRequest request) throws InterruptedException {
+        DeferredResult<String> result = new DeferredResult<>();
+        result.onCompletion(() -> System.out.println("调用完成"));
+//        new Thread(() -> result.setResult("handler")).start();
+        return result;
     }
 
     @GetMapping("/body")
