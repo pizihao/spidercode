ddc7e016cb62aa0b5e6f47001ad7df6dc0f7b9e5
pizihao <2335715300@qq.com>
2022-09-23 19:07:50 +0800

    使用flink初试统计的案例

--- flink/src/main/java/com/deep/flink/DataConvert.java
+++ flink/src/main/java/com/deep/flink/DataConvert.java
@@ -1,6 +1,8 @@
 package com.deep.flink;
 
 import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -33,6 +35,18 @@ public class DataConvert {
             String[] next = iterator.next();
             System.out.println(Arrays.toString(next));
         }
+    }
+
+
+    public void test(){
+        ExecutionEnvironment environment = ExecutionEnvironment.getExecutionEnvironment();
+        //注意 readTextFile路径填你自己的文件路径。
+        DataSource<String> testDataSet = environment.readTextFile("1.txt");
+        //可以理解testDataSet为一个RDD
+        //groupby(0)意思是以元组第一个为key进行分组，sum(1)是对元组第二个位置数据进行累加。
+        val result = testDataSet.flatMap(_.split(" ")).map((_, 1)).groupBy(0).sum(1)
+        result.print()
+    }
     }
 
 }
