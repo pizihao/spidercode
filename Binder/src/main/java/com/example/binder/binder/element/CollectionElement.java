f1fb5b7a92149606ac39f9dc4be5eb18713c6fc8
pizihao <2335715300@qq.com>
2022-10-12 19:05:51 +0800

    binder 的基础结构

---
+++ Binder/src/main/java/com/example/binder/binder/element/CollectionElement.java
@@ -0,0 +1,7 @@
+package com.example.binder.binder.element;
+
+/**
+ * @author Create by liuwenhao on 2022/10/12 16:12
+ */
+public class CollectionElement implements Element{
+}
---
+++ Binder/src/main/java/com/example/binder/binder/mapper/DefaultSourceMapper.java
@@ -0,0 +1,17 @@
+package com.example.binder.binder.mapper;
+
+/**
+ * @author Create by liuwenhao on 2022/10/12 17:07
+ */
+public class DefaultSourceMapper implements SourceMapper {
+    /**
+     * 默认将 - 符号和 _ 符号转化为驼峰
+     *
+     * @param s 转换前的key
+     * @return 转换后的key，驼峰形式
+     */
+    @Override
+    public String convert(String s) {
+        return null;
+    }
+}
