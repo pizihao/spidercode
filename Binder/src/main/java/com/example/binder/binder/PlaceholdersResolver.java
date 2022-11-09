8313f5258e076a9033c9f647e399a0c5f42f0d8f
pizihao <2335715300@qq.com>
2022-11-09 10:46:17 +0800

    删除重复的无意义代码

--- Binder/src/main/java/com/example/binder/binder/PlaceholdersResolver.java
+++
@@ -1,9 +0,0 @@
-package com.example.binder.binder;
-
-/**
- * 处理占位符，配置文件中允许使用{@code ${}}引用当前上下文的数据
- *
- * @author Create by liuwenhao on 2022/10/12 17:14
- */
-public class PlaceholdersResolver {
-}
--- Binder/src/main/java/com/example/binder/binder/source/MapPropertiesSource.java
+++
@@ -1,7 +0,0 @@
-package com.example.binder.binder.source;
-
-/**
- * @author Create by liuwenhao on 2022/10/12 16:33
- */
-public class MapPropertiesSource implements Source {
-}
