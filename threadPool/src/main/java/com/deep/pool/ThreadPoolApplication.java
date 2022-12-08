60e51248dff42fc4c23409361f7df2c8746347b0
pizihao <2335715300@qq.com>
2022-12-08 17:16:08 +0800

    添加启动参数实例

--- threadPool/src/main/java/com/deep/pool/ThreadPoolApplication.java
+++ threadPool/src/main/java/com/deep/pool/ThreadPoolApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+/**
+ * -XX:+PrintGC -Xmn880M -Xms2048M -XX:SurvivorRatio=8 -XX:MetaspaceSize=256M -XX:+UseConcMarkSweepGC -XX:+UseCMSCompactAtFullCollection
+ * -Drebel.notification.url=http://localhost:17434 -javaagent:C:\SoftWare\jetbrains\apps\IDEA-U\ch-0\223.7571.182.plugins\jr-ide-idea\lib\xrebel\xrebel.jar
+ */
 @SpringBootApplication
 @EnableDynamicThreadPool
 public class ThreadPoolApplication {
