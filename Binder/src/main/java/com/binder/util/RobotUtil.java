69e009920280661562d04cb965e96982706b7bbe
pizihao <2335715300@qq.com>
2023-01-05 15:19:16 +0800

    移动鼠标测试

---
+++ Binder/src/main/java/com/binder/util/RobotUtil.java
@@ -0,0 +1,12 @@
+package com.binder.util;
+
+import java.awt.*;
+
+public class RobotUtil {
+
+    public static void main(String[] args) throws AWTException {
+        Robot robot = new Robot();
+        robot.mouseMove(10, 1000);
+    }
+
+}
