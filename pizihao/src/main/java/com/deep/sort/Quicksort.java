cf535a69b91273772b8ecbeca4256c8ac401883a
pizihao <2335715300@qq.com>
2023-01-19 22:38:34 +0800

    快速排序

--- pizihao/src/main/java/com/deep/sort/Quicksort.java
+++ pizihao/src/main/java/com/deep/sort/Quicksort.java
@@ -9,10 +9,30 @@ public class Quicksort {
     }
 
 
-    public static int[] quickSort(int[] ints){
-
-
-        return new int[]{};
+    private void quickSort(int[] src, int begin, int end) {
+        if (begin < end) {
+            int key = src[begin];
+            int i = begin;
+            int j = end;
+            while (i < j) {
+                while (i < j && src[j] > key) {
+                    j--;
+                }
+                if (i < j) {
+                    src[i] = src[j];
+                    i++;
+                }
+                while (i < j && src[i] < key) {
+                    i++;
+                }
+                if (i < j) {
+                    src[j] = src[i];
+                    j--;
+                }
+            }
+            src[i] = key;
+            quickSort(src, begin, i - 1);
+            quickSort(src, i + 1, end);
+        }
     }
-
 }
