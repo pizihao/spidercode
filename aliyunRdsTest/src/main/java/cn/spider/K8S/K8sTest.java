cbf8183998f3d6d8f5f17411e8e2754fc8d4cb9f
liuwenhao <2335715300@qq.com>
2023-04-11 17:45:04 +0800

    测试数字格式

--- aliyunRdsTest/src/main/java/cn/spider/K8S/K8sTest.java
+++ aliyunRdsTest/src/main/java/cn/spider/K8S/K8sTest.java
@@ -1,11 +1,14 @@
 package cn.spider.K8S;
 
 import io.fabric8.kubernetes.api.model.PodList;
+import io.fabric8.kubernetes.api.model.Quantity;
 import io.fabric8.kubernetes.client.KubernetesClient;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class K8sTest {
 
@@ -17,6 +20,22 @@ public class K8sTest {
         boolean isAllReady = readies.stream()
                 .anyMatch(c -> !c.getReady());
         System.out.println(isAllReady);
+
+        Float cpuTotal = Optional.ofNullable("1.22")
+                .map(Float::valueOf)
+                .map(a -> {
+                    DecimalFormat format = new DecimalFormat("#.#");
+                    return format.format(a);
+                })
+                .map(Float::valueOf)
+                .get();
+        System.out.println(cpuTotal);
+
+
+        DecimalFormat format = new DecimalFormat("#.#");
+        System.out.println(format.format(1.266d));
+        System.out.println(format.format(1.6005d));
+        System.out.println(format.format(451.266d));
 
     }
 
