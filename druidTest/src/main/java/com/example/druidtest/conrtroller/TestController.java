d78bace799bc21aac00e0265f4eb071943420abc
liuwenhao <2335715300@qq.com>
2023-04-20 13:21:30 +0800

    画图和循环任务

--- druidTest/src/main/java/com/example/druidtest/conrtroller/TestController.java
+++ druidTest/src/main/java/com/example/druidtest/conrtroller/TestController.java
@@ -5,13 +5,15 @@ import com.example.druidtest.model.DeriveDO;
 import com.example.druidtest.model.Model;
 import com.example.druidtest.model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.locks.LockSupport;
 
 /**
  * <h2></h2>
@@ -25,6 +27,8 @@ public class TestController {
 
     @Autowired
     DeriveMapper deriveMapper;
+    @Autowired
+    DataSource dataSource;
 
     @GetMapping(value = "/{id}")
     public Callable<String> test(@PathVariable String id, HttpServletRequest request) throws InterruptedException {
@@ -72,6 +76,43 @@ public class TestController {
 
         deriveMapper.add(list);
 
+
+        return "test";
+    }
+
+    @GetMapping("/testTime")
+    public long testTime() {
+        String sql = "SELECT * FROM `dept`;";
+        String sql1 = "SHOW TABLE STATUS LIKE 'dept';";
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement();) {
+            ResultSet rs = statement.executeQuery("EXPLAIN " + sql);
+            // 统计每个步骤所需的时间
+            long scanRows = 0;
+            long ioOperations = 0;
+            while (rs.next()) {
+                String type = rs.getString("type");
+                long rows = rs.getLong("rows");
+                if ("range".equals(type) || "ref".equals(type) || "index".equals(type)) {
+                    scanRows += rows;
+                }
+                ioOperations += rows;
+            }
+            // 计算预估的 SQL 执行时间
+            long estimatedTime = (scanRows * 10 + ioOperations * 50) / 1000;
+            System.out.println("Estimated execution time: " + estimatedTime);
+            return estimatedTime;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new UnsupportedOperationException(e);
+        }
+    }
+
+    @GetMapping("/async")
+    public String async() {
+        deriveMapper.async();
+                   //           1000000000L
+        LockSupport.parkNanos(600000000000L);
 
         return "test";
     }
