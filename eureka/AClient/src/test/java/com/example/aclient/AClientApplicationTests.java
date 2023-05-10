b26ca5ea995346d5adf9f4b43ed95226509fed45
liuwenhao <2335715300@qq.com>
2023-05-10 09:05:00 +0800

    eureka测试

---
+++ eureka/AClient/src/test/java/com/example/aclient/AClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eureka/BClient/src/test/java/com/example/bclient/BClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
